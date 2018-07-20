e5322c64ec950099f59322daf8f20d6c9783d856
Andrei Egorov <andreyegorov1992@gmail.com>
2018-07-20 09:20:38 -0700

    Add Boomtown logo

--- client/src/pages/Items/Items.js
+++ client/src/pages/Items/Items.js
@@ -9,72 +9,33 @@ import styles from './styles'
 
 
 import ItemsContainer from '../../containers/ItemsContainer';
+import HeaderBar from '../../components/HeaderBar';
 
 
 
-
-
-//<ItemsContainer>
-//       {({ itemsData: { items, loading, error } }) => {
-//        return loading
-//        ? (
-//        <p> Loading... </p>/* Display a loading component */
-//    )
-//  : (
-//  <ul>items.map()m........</ul>
-//);
-//}}
-//</ItemsContainer>
 
 const Items = ({ classes }) => {
   return (
-    <ItemsContainer>
+    <div>
+      <HeaderBar />
+      <ItemsContainer>
 
-      {({ itemsData: { data, loading, error } }) => {
-        if (loading) return '...loading'
-        if (error) return '...error'
-        return data.items.map((item, index) => <div key={index}>
-          <h2>{item.title}</h2>
-          <p>{item.tags.map(tag => tag.title)}</p>
-          <p>{item.description}</p>
-        </div>)
-        console.log(data)
-      }
-
-
-
-      }
-    </ItemsContainer>
+        {({ itemsData: { data, loading, error } }) => {
+          if (loading) return '...loading'
+          if (error) return '...error'
+          return data.items.map((item, index) => <div key={index}>
+            <h2>{item.title}</h2>
+            <p>{item.tags.map(tag => tag.title)}</p>
+            <p>{item.description}</p>
+          </div>)
+          console.log(data)
+        }
+        }
+      </ItemsContainer>
+    </div>
   )
 }
 
 export default withStyles(styles)(Items)
 
 
-// return <div><pre>{JSON.stringify(data, null, 4)}</pre></div>
-// {
-//   ({ itemsData: { items, loading, error } }) => {
-//     if (loading) return '...loading'
-//     if (error) return '...error'
-//     return (
-//       items.map(item => {
-//         return <div>{item.title}</div>
-//       })
-
-//     )
-
-
-//     {/* return loading ? (return  'Loading...') : error ? (return '...error'): (
-//           items.map((item) => (<ul><li>{item} </li> </ul>)
-//           )) */}
-
-
-//     return loading ? (<p> Loading... </p>/* Display a loading component */)
-//       : error ? (<p> Error... </p>)
-//         : (
-
-//           data.map((item) => <p>{item.title}</p>),
-//           console.log(item)
-
-
-//         );
