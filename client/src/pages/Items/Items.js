4ff7ac9ff3050dd3afa00d0e31afb7c22d98e716
Andrei Egorov <andreyegorov1992@gmail.com>
2018-07-22 20:21:20 -0700

    Style Item cards

--- client/src/pages/Items/Items.js
+++ client/src/pages/Items/Items.js
@@ -1,4 +1,3 @@
-
 import { withStyles } from '@material-ui/core/styles'
 import React from 'react'
 
@@ -6,40 +5,46 @@ import Bar from './bar'
 
 import styles from './styles'
 
+import ItemsContainer from '../../containers/ItemsContainer'
+import HeaderBar from '../../components/HeaderBar'
+import ItemCard from '../../components/ItemCard'
 
-
-import ItemsContainer from '../../containers/ItemsContainer';
-import HeaderBar from '../../components/HeaderBar';
-import ItemCard from '../../components/ItemCard';
-
-
+import Grid from '@material-ui/core/Grid'
 
 const Items = ({ classes }) => {
   return (
     <div>
-      <HeaderBar/>
-      
-      <ItemsContainer>
-      
-        {({ itemsData: { data, loading, error } }) => {
-          if (loading) return '...loading'
-          if (error) return '...error'
-          const items = data.items
-          return items.map((item, index) => <div key={index}>
-            <ItemCard item={item}/>
-          </div>)
-        }
+      <HeaderBar />
 
-        
-
-        }
-        
-      </ItemsContainer>
-      
+      <Grid
+        container
+        align="center"
+        justify="center"
+        className={classes.gridContainer}
+      >
+        <ItemsContainer>
+          {({ itemsData: { data, loading, error } }) => {
+            if (loading) return '...loading'
+            if (error) return '...error'
+            const items = data.items
+            return items.map((item, index) => (
+              <Grid
+                key={index}
+                item
+                xs={12}
+                sm={12}
+                md={6}
+                lg={4}
+                className={classes.cardCell}
+              >
+                <ItemCard item={item} />
+              </Grid>
+            ))
+          }}
+        </ItemsContainer>
+      </Grid>
     </div>
   )
 }
 
 export default withStyles(styles)(Items)
-
-
--- client/src/pages/Items/styles.js
+++ client/src/pages/Items/styles.js
@@ -1,3 +1,14 @@
-const styles = theme => ({})
+const styles = theme => ({
+  gridContainer: {
+    backgroundColor: 'red',
+    padding: '80px',
+    justifyContent: 'flex-start'
+    // display: "flex",
+    // justifyContent:"center",
+  },
+  cardCell: {
+    padding: '12px'
+  }
+})
 
 export default styles
