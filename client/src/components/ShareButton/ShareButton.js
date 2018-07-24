797d923cdba89ed9845f07bcda7abf70f8d14ca6
Andrei Egorov <andreyegorov1992@gmail.com>
2018-07-23 18:51:20 -0700

    Add a link to a share button

--- client/src/components/ShareButton/ShareButton.js
+++ client/src/components/ShareButton/ShareButton.js
@@ -5,7 +5,7 @@ import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import Icon from '@material-ui/core/Icon';
 import AddCircle from '@material-ui/icons/AddCircle';
-
+import { Link } from 'react-router-dom'
 import styles from './styles';
 
 
@@ -16,7 +16,9 @@ function ShareButton(props) {
   return (
     <div >
      
-      <Button variant="extendedFab" aria-label="Delete" className={classes.button}>
+   
+   
+      <Button variant="extendedFab" aria-label="Delete" className={classes.button} component={Link} to="/share">
        <AddCircle className={classes.circle}/>
           Share Something
       </Button>
